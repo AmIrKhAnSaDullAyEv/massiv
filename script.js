cb2f4f1e1208d70ffb0b7ab33bf4eeddc5140fa6
Amirkhan <amirjonsadullayev@gmail.com>
2021-12-10 16:14:26 +0500

    massiv

---
+++ script.js
@@ -0,0 +1,15 @@
+let num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+
+console.log('четные числа');
+for (let i = 1; i < num.length; i++){
+    console.log(num[i]);
+    i = i + 1;
+}
+
+
+console.log('нечетные числа');
+for (let i = 0; i < num.length; i++){
+    console.log(num[i]);
+    i = i + 1;
+}
